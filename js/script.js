4e2bef951ddf3612a6c30052873ffa55aed35481
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-01-30 21:14:11 -0300

    JS dark mode done

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const hourElement = document.querySelector('.hour')
+const minuteElement = document.querySelector('.minute')
+const secondElement = document.querySelector('.second')
+const timeElement = document.querySelector('.time')
+const dateElement = document.querySelector('.date')
+const toggle = document.querySelector('.toggle')
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "May", "Apr", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+//dark mode
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark Mode'
+
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light Mode'
+    }
+})
+
